a606f6e26d014701b20a13513e677af2c5f8caa8
sunjiahaoz <sunjiahaoz@126.com>
2017-06-13 20:36:44 +0800

    完成

---
+++ Assets/Demo/Demo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using KK.Frame.Persist;
+
+public class Demo : MonoBehaviour {
+    IPersist persist = new PlayerPrefsPersist();
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("SaveInt", GUILayout.Width(100), GUILayout.Height(100)))
+        {
+            persist.SetInt("intKey", 999);            
+            Debug.Log("<color=green>[log]</color>---" + "saveInt");
+        }
+        if (GUILayout.Button("GetInt", GUILayout.Width(100), GUILayout.Height(100)))
+        {
+            if (!persist.HasKey("intKey"))
+            {
+                Debug.LogWarning("<color=orange>[Warning]</color>---" + "不存在intKey");
+            }
+            else
+            {
+                Debug.Log("<color=green>[log]</color>---" + persist.GetInt("intKey"));
+            }
+        }
+        if (GUILayout.Button("deleteInt", GUILayout.Width(100), GUILayout.Height(100)))
+        {
+            persist.DeleteKey("intKey");
+            Debug.Log("<color=green>[log]</color>---" + "deleteInt");
+        }
+        if (GUILayout.Button("deleteAll", GUILayout.Width(100), GUILayout.Height(100)))
+        {
+            persist.DeleteAll();
+            Debug.Log("<color=green>[log]</color>---" + "deleteAll");
+        }
+
+    }
+}
